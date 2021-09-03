009ccc68484e737e1bd8e489860e8fc84ab69ca6
Whit Stroup <49410964+whitstroup@users.noreply.github.com>
2021-09-03 14:53:49 -0500

    Update Program.cs

--- RefandOut/Program.cs
+++ RefandOut/Program.cs
@@ -4,7 +4,7 @@ namespace RefandOut
 {
     class Program
     {
-        //TODO impliment this ref method by returning a bool and subtracting from the parameter.
+        //TODO implement this ref method by returning a bool and subtracting from the parameter.
         //So it will have two outputs. the bool of true if health is > 0. and also set x to decrease by 1.
         //TODO Call the method in the main and make sure the method changes the number of a initialized variable and returns a bool.
         public static bool SubtractHealth(ref int x)
@@ -17,7 +17,7 @@ namespace RefandOut
 
         public int Sum(int x, int y, out  bool even)
         {
-            //use this metho or make your own.
+            //use this method or make your own.
 
         }
 
